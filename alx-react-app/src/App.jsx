9086557b3793fc93d9f751953987e9f067e18984
Mutesasira Muwada <mutesasiramuwada503@gmail.com>
2025-09-03 16:36:53 +0300

    Move components to components directory and update imports

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,15 @@
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Header from './components/Header.jsx';
+import MainContent from './components/MainContent.jsx';
+import Footer from './components/Footer.jsx';
+import './App.css';
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Header />
       <MainContent />
       <Footer />
-    </>
+    </div>
   );
 }
 

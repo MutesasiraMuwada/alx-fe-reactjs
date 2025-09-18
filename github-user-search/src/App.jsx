37b903c60a4e4f73f319d4b2c0545f84666f808a
Mutesasira Muwada <mutesasiramuwada503@gmail.com>
2025-09-18 13:27:00 +0300

    Task 2: Enhance Search component UI and add advanced search features

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div>
-      <h1>GitHub User Search App</h1>
-      <p>Setup complete!</p>
+      <Search />
     </div>
   );
 }
